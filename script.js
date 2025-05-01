be2e2d863ce57531e90f7218b3446df1a5c6ffb7
Husni786 <133058247+Husni786@users.noreply.github.com>
2025-05-01 23:38:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Mobile Menu Toggle
-document.getElementById('mobileMenu').addEventListener('click', function() {
-  const navCollapse = document.querySelector('.navbar-collapse');
-  navCollapse.classList.toggle('show');
-});
-
-// Image Slider (Example)
-// Add a carousel for featured articles if needed
